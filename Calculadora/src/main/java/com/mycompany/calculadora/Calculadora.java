bf6d17bfbf7213087de19163fef426093932b968
CristhianRosales <141367540+CristhianRosales@users.noreply.github.com>
2023-08-10 13:41:55 -0500

    Update Calculadora.java

--- Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
+++ Calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -1,16 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.calculadora;
-
-/**
- *
- * @author Estudiante
- */
+import java.lang.Math;
+import java.util.Scanner;
 public class Calculadora {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        float a, b, resultado;
+        char operacion;
+        resultado=0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Binevenido a la calculadora Java\nDigite un valor y presione enter, digite una operacion (+, -, *, /) y presione enter, digite un valor y presione enter para obtener el resultado.\n");
+        a=in.nextInt();
+        operacion=in.next().charAt(0);
+        b=in.nextInt();
+        switch(operacion){
+            case '+':resultado=a+b;
+                     break;
+            case '-':resultado=a-b;
+                     break;
+            case '*':resultado=a*b;
+                     break;
+            case '/':resultado=a/b;
+                     break;
+        }
+        System.out.println("El resultado es: " + resultado);
     }
 }
